f5892761ef10395e069d93b3657dcba9b94bc8b3
fushenshen <hellofss@foxmail.com>
2023-01-07 22:17:52 +0800

    增加多batch的性能benchmark

--- bench/bench_net.cpp
+++ bench/bench_net.cpp
@@ -84,9 +84,30 @@ static void BM_Yolov5nano(benchmark::State &state) {
   }
 }
 
+static void BM_Yolov5s(benchmark::State &state) {
+  using namespace kuiper_infer;
+  RuntimeGraph graph("tmp/yolo/demo/yolov5s_batch4.pnnx.param",
+                     "tmp/yolo/demo/yolov5s_batch4.pnnx.bin");
+
+  graph.Build("pnnx_input_0", "pnnx_output_0");
+  const uint32_t batch_size = 4;
+  std::vector<std::shared_ptr<Tensor<float>>> inputs;
+
+  for (int i = 0; i < batch_size; ++i) {
+    std::shared_ptr<Tensor<float>> input = std::make_shared<Tensor<float>>(3, 640, 640);
+    input->Ones();
+    inputs.push_back(input);
+  }
+
+  for (auto _ : state) {
+    std::vector<std::shared_ptr<Tensor<float>>> outputs = graph.Forward(inputs, false);
+  }
+}
+
 BENCHMARK(BM_Resnet18)->Iterations(kIterationNum);
 BENCHMARK(BM_Resnet18_Batch16)->Iterations(kIterationNum);
 BENCHMARK(BM_MobilenetV3)->Iterations(kIterationNum);
 BENCHMARK(BM_Yolov5nano)->Iterations(kIterationNum);
+BENCHMARK(BM_Yolov5s)->Iterations(kIterationNum);
 
 BENCHMARK_MAIN();
