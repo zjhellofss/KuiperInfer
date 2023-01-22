cdcee77265e75f84eb06c02e61a4f45f4ee927b7
fushenshen <hellofss@foxmail.com>
2023-01-22 10:14:59 +0800

    增加layer_factory的测试

---
+++ test/test_layer_factory.cpp
@@ -0,0 +1,43 @@
+//
+// Created by fss on 23-1-22.
+//
+
+#include <gtest/gtest.h>
+#include "layer/abstract/layer_factory.hpp"
+#include "layer/abstract/layer.hpp"
+#include "../source/layer/details/relu.hpp"
+
+using namespace kuiper_infer;
+ParseParameterAttrStatus TestCreateLayer(const std::shared_ptr<RuntimeOperator> &op,
+                                         std::shared_ptr<Layer> &layer) {
+  layer = std::make_shared<Layer>("test3");
+  return ParseParameterAttrStatus::kParameterAttrParseSuccess;
+}
+
+TEST(test_layer_factory, init) {
+  using namespace kuiper_infer;
+  const auto &r1 = LayerRegisterer::Registry();
+  const auto &r2 = LayerRegisterer::Registry();
+  ASSERT_EQ(r1, r2);
+}
+
+TEST(test_layer_factory, registerer) {
+  using namespace kuiper_infer;
+  const uint32_t size1 = LayerRegisterer::Registry().size();
+  LayerRegisterer::RegisterCreator("test1", TestCreateLayer);
+  const uint32_t size2 = LayerRegisterer::Registry().size();
+  LayerRegisterer::RegisterCreator("test2", TestCreateLayer);
+  const uint32_t size3 = LayerRegisterer::Registry().size();
+
+  ASSERT_EQ(size2 - size1, 1);
+  ASSERT_EQ(size3 - size1, 2);
+}
+
+TEST(test_layer_factory, create) {
+  using namespace kuiper_infer;
+  LayerRegisterer::RegisterCreator("test3", TestCreateLayer);
+  std::shared_ptr<RuntimeOperator> op = std::make_shared<RuntimeOperator>();
+  op->type = "test3";
+  std::shared_ptr<Layer> layer = LayerRegisterer::CreateLayer(op);
+  ASSERT_EQ(layer->layer_name(),"test3");
+}
--- test/test_tensor.cpp
+++ test/test_tensor.cpp
@@ -309,7 +309,7 @@ TEST(test_tensor, raw_view3) {
   ASSERT_EQ(shapes.at(2), 2);
 }
 
-TEST(test_tensor, padding) {
+TEST(test_tensor, padding1) {
   using namespace kuiper_infer;
   Tensor<float> tensor(3, 4, 5);
   ASSERT_EQ(tensor.channels(), 3);
@@ -327,6 +327,36 @@ TEST(test_tensor, padding) {
       for (int c_ = 0; c_ < tensor.cols(); ++c_) {
         if (c_ == 0 || r == 0) {
           ASSERT_EQ(tensor.at(c, r, c_), 0);
+        }
+        index += 1;
+      }
+    }
+  }
+}
+
+TEST(test_tensor, padding2) {
+  using namespace kuiper_infer;
+  ftensor tensor(3, 4, 5);
+  ASSERT_EQ(tensor.channels(), 3);
+  ASSERT_EQ(tensor.rows(), 4);
+  ASSERT_EQ(tensor.cols(), 5);
+
+  tensor.Fill(1.f);
+  tensor.Padding({2, 2, 2, 2}, 3.14f);
+  ASSERT_EQ(tensor.rows(), 8);
+  ASSERT_EQ(tensor.cols(), 9);
+
+  int index = 0;
+  for (int c = 0; c < tensor.channels(); ++c) {
+    for (int r = 0; r < tensor.rows(); ++r) {
+      for (int c_ = 0; c_ < tensor.cols(); ++c_) {
+        if (c_ <= 1 || r <= 1) {
+          ASSERT_EQ(tensor.at(c, r, c_), 3.14f);
+        } else if (c >= tensor.cols() - 1 || r >= tensor.rows() - 1) {
+          ASSERT_EQ(tensor.at(c, r, c_), 3.14f);
+        }
+        if ((r >= 2 && r <= 5) && (c_ >= 2 && c_ <= 6)) {
+          ASSERT_EQ(tensor.at(c, r, c_), 1.f);
         }
         index += 1;
       }
