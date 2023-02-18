04ce0a36296952323993f4456ba7b2758b91fb38
fushenshen <hellofss@foxmail.com>
2023-02-18 22:43:01 +0800

    修改test_cat单元测试

--- test/test_layer/test_cat.cpp
+++ test/test_layer/test_cat.cpp
@@ -8,19 +8,17 @@
 
 TEST(test_layer, cat1) {
   using namespace kuiper_infer;
-  int input_size = 8;
-  int input_channels = 4;
-
+  int input_size = 4;
   int output_size = 2;
-  int output_channels = 16;
+  int input_channels = 6;
+  int output_channels = 12;
 
   std::vector<std::shared_ptr<Tensor<float>>> inputs;
   for (int i = 0; i < input_size; ++i) {
     std::shared_ptr<Tensor<float>> input = std::make_shared<Tensor<float>>(input_channels, 32, 32);
-    input->Fill(float(i));
+    input->Rand();
     inputs.push_back(input);
   }
-
   std::vector<std::shared_ptr<Tensor<float>>> outputs(output_size);
   CatLayer cat_layer(1);
   const auto status = cat_layer.Forward(inputs, outputs);
@@ -29,25 +27,27 @@ TEST(test_layer, cat1) {
     ASSERT_EQ(outputs.at(i)->channels(), output_channels);
   }
 
-  for (uint32_t i = 0; i < input_size; ++i) {
-    sftensor input = inputs.at(i);
-    for (uint32_t j = 0; j < input_channels; ++j) {
-      const arma::fmat &in_channel = input->at(j);
-      uint32_t index = i * input_channels + j;
-      uint32_t i_index = index / output_channels;
-      uint32_t j_index = index % output_channels;
-      const arma::fmat &out_channel = outputs.at(i_index)->at(j_index);
+  for (int i = 0; i < input_size / 2; ++i) {
+    for (int input_channel = 0; input_channel < input_channels; ++input_channel) {
+      ASSERT_TRUE(arma::approx_equal(inputs.at(i)->at(input_channel),
+                                     outputs.at(i)->at(input_channel), "absdiff", 0.01f));
+    }
+  }
+
+  for (int i = input_size / 2; i < input_size; ++i) {
+    for (int input_channel = input_channels; input_channel < input_channels * 2; ++input_channel) {
+      ASSERT_TRUE(arma::approx_equal(inputs.at(i)->at(input_channel - input_channels),
+                                     outputs.at(i - output_size)->at(input_channel), "absdiff", 0.01f));
     }
   }
 }
 
 TEST(test_layer, cat2) {
   using namespace kuiper_infer;
-  int input_size = 16;
-  int input_channels = 32;
-
-  int output_size = 2;
-  int output_channels = 256;
+  int input_size = 8;
+  int output_size = 4;
+  int input_channels = 64;
+  int output_channels = 128;
 
   std::vector<std::shared_ptr<Tensor<float>>> inputs;
   for (int i = 0; i < input_size; ++i) {
@@ -55,7 +55,6 @@ TEST(test_layer, cat2) {
     input->Rand();
     inputs.push_back(input);
   }
-
   std::vector<std::shared_ptr<Tensor<float>>> outputs(output_size);
   CatLayer cat_layer(1);
   const auto status = cat_layer.Forward(inputs, outputs);
@@ -64,85 +63,17 @@ TEST(test_layer, cat2) {
     ASSERT_EQ(outputs.at(i)->channels(), output_channels);
   }
 
-  for (uint32_t i = 0; i < input_size; ++i) {
-    sftensor input = inputs.at(i);
-    for (uint32_t j = 0; j < input_channels; ++j) {
-      const arma::fmat &in_channel = input->at(j);
-      uint32_t index = i * input_channels + j;
-      uint32_t i_index = index / output_channels;
-      uint32_t j_index = index % output_channels;
-      const arma::fmat &out_channel = outputs.at(i_index)->at(j_index);
+  for (int i = 0; i < input_size / 2; ++i) {
+    for (int input_channel = 0; input_channel < input_channels; ++input_channel) {
+      ASSERT_TRUE(arma::approx_equal(inputs.at(i)->at(input_channel),
+                                     outputs.at(i)->at(input_channel), "absdiff", 0.01f));
+    }
+  }
+
+  for (int i = input_size / 2; i < input_size; ++i) {
+    for (int input_channel = input_channels; input_channel < input_channels * 2; ++input_channel) {
+      ASSERT_TRUE(arma::approx_equal(inputs.at(i)->at(input_channel - input_channels),
+                                     outputs.at(i - output_size)->at(input_channel), "absdiff", 0.01f));
     }
   }
 }
-
-TEST(test_layer, cat3) {
-  using namespace kuiper_infer;
-  int input_size = 16;
-  int input_channels = 32;
-
-  int output_size = 1;
-  int output_channels = 512;
-
-  std::vector<std::shared_ptr<Tensor<float>>> inputs;
-  for (int i = 0; i < input_size; ++i) {
-    std::shared_ptr<Tensor<float>> input = std::make_shared<Tensor<float>>(input_channels, 32, 32);
-    input->Rand();
-    inputs.push_back(input);
-  }
-
-  std::vector<std::shared_ptr<Tensor<float>>> outputs(output_size);
-  CatLayer cat_layer(1);
-  const auto status = cat_layer.Forward(inputs, outputs);
-  ASSERT_EQ(status, InferStatus::kInferSuccess);
-  for (uint32_t i = 0; i < outputs.size(); ++i) {
-    ASSERT_EQ(outputs.at(i)->channels(), output_channels);
-  }
-
-  for (uint32_t i = 0; i < input_size; ++i) {
-    sftensor input = inputs.at(i);
-    for (uint32_t j = 0; j < input_channels; ++j) {
-      const arma::fmat &in_channel = input->at(j);
-      uint32_t index = i * input_channels + j;
-      uint32_t i_index = index / output_channels;
-      uint32_t j_index = index % output_channels;
-      const arma::fmat &out_channel = outputs.at(i_index)->at(j_index);
-    }
-  }
-}
-
-TEST(test_layer, cat4) {
-  using namespace kuiper_infer;
-  int input_size = 320;
-  int input_channels = 160;
-
-  int output_size = 160;
-  int output_channels = 320;
-
-  std::vector<std::shared_ptr<Tensor<float>>> inputs;
-  for (int i = 0; i < input_size; ++i) {
-    std::shared_ptr<Tensor<float>> input = std::make_shared<Tensor<float>>(input_channels, 32, 32);
-    input->Rand();
-    inputs.push_back(input);
-  }
-
-  std::vector<std::shared_ptr<Tensor<float>>> outputs(output_size);
-  CatLayer cat_layer(1);
-  const auto status = cat_layer.Forward(inputs, outputs);
-  ASSERT_EQ(status, InferStatus::kInferSuccess);
-  for (uint32_t i = 0; i < outputs.size(); ++i) {
-    ASSERT_EQ(outputs.at(i)->channels(), output_channels);
-  }
-
-  for (uint32_t i = 0; i < input_size; ++i) {
-    sftensor input = inputs.at(i);
-    for (uint32_t j = 0; j < input_channels; ++j) {
-      const arma::fmat &in_channel = input->at(j);
-      uint32_t index = i * input_channels + j;
-      uint32_t i_index = index / output_channels;
-      uint32_t j_index = index % output_channels;
-      const arma::fmat &out_channel = outputs.at(i_index)->at(j_index);
-    }
-  }
-}
-
