2c81a0213cf097164a5cf04bad33a909441f5679
zjhellofss <hellofss@foxmail.com>
2023-09-23 17:20:38 +0800

    修改runtime_ir中的代码注释

--- include/runtime/runtime_ir.hpp
+++ include/runtime/runtime_ir.hpp
@@ -33,135 +33,221 @@
 
 namespace kuiper_infer {
 
-/// 计算图结构，由多个计算节点和节点之间的数据流图组成
+/**
+ * @brief Runtime representation of a computation graph
+ *
+ * This class represents a computation graph at runtime. It initializes
+ * the graph from parameter and weights files, and allows setting
+ * inputs and executing the graph.
+ */
 class RuntimeGraph {
  public:
   /**
-   * 初始化计算图
-   * @param param_path 计算图的结构文件
-   * @param bin_path 计算图中的权重文件
+   * @brief Construct a new RuntimeGraph object
+   *
+   * Creates a runtime graph representation from parameter and bin files.
+   *
+   * @param param_path Path to the parameter file defining the graph structure
+   * @param bin_path Path to the bin file containing the graph weights
    */
   RuntimeGraph(std::string param_path, std::string bin_path);
 
+  /**
+   * @brief Sets the inputs to the graph
+   *
+   * Sets the input tensors for executing the graph.
+   *
+   * @param input_name Name of the input
+   * @param inputs Vector of input tensors
+   */
   void set_inputs(const std::string& input_name,
                   const std::vector<sftensor>& inputs);
 
+  /**
+   * @brief Gets output tensors from the graph
+   *
+   * Returns the output tensors with the given name.
+   *
+   * @param output_name Name of the graph output
+   * @return Vector of output tensors
+   */
   std::vector<sftensor> get_outputs(const std::string& output_name) const;
 
+  /**
+   * @brief Checks if an op is an input op
+   *
+   * Returns true if the given op name corresponds to an input operation.
+   *
+   * @param op_name Name of the op to check
+   * @return True if the op is an input op, false otherwise
+   */
   bool is_input_op(const std::string& op_name) const;
 
+  /**
+   * @brief Checks if an op is an output op
+   *
+   * Returns true if the given op name is an output operation.
+   *
+   * @param op_name Name of the op to check
+   * @return True if the op is an output op, false otherwise
+   */
   bool is_output_op(const std::string& op_name) const;
 
   /**
-   * 构建计算图
-   * @param input_name 计算图输入节点的名称
-   * @param output_name  计算图输出节点的名称
+   * @brief Builds the computation graph
+   *
+   * Performs the actual construction of the graph based on the provided
+   * parameter and weights files. This must be called after
+   * initing the the graph and before executing the graph.
    */
   void Build();
 
   /**
-   * 设置权重文件
-   * @param bin_path 权重文件路径
+   * @brief Sets the path to the weights file
+   *
+   * @param bin_path Path to the weights file
    */
   void set_bin_path(const std::string& bin_path);
 
   /**
-   * 设置结构文件
-   * @param param_path  结构文件路径
+   * @brief Sets the path to the parameter file
+   *
+   * @param param_path Path to the parameter file
    */
   void set_param_path(const std::string& param_path);
 
   /**
-   * 返回结构文件
-   * @return 返回结构文件
+   * @brief Gets the parameter file path
+   *
+   * @return The path to the parameter file
    */
   const std::string& param_path() const;
 
   /**
-   * 返回权重文件
-   * @return 返回权重文件
+   * @brief Gets the weights file path
+   *
+   * @return The path to the weights file
    */
   const std::string& bin_path() const;
 
   /**
-   * 计算图的执行,根据深度优先搜索的顺序执行
-   * @param debug 是否调试，如果调试则输出一些中间信息
+   * @brief Executes the computation graph
+   *
+   * Executes the graph operations in depth-first order.
+   *
+   * @param debug Whether to print debugging information during execution
    */
   void Forward(bool debug = false);
 
  private:
   /**
-   * 计算图的初始化
-   * @return 是否初始化成功
+   * @brief Initializes the graph
+   *
+   * Initializes the graph from the provided parameter and weights files.
+   *
+   * @return True if initialization succeeded, false otherwise
    */
   bool Init();
 
   /**
-   * 构建模型的拓扑执行顺序
+   * @brief Performs reverse topological sort on the graph
+   *
+   * Sorts the graph operators in reverse topological order for execution.
    */
   void ReverseTopoSort();
 
   /**
-   * 构建模型的拓扑执行顺序
+   * @brief Recursive reverse topological sort
+   *
+   * Recursive helper for reverse topological sort algorithm.
+   *
+   * @param root_op Root operator to start sort from
    */
   void ReverseTopoSort_(const std::shared_ptr<RuntimeOperator>& root_op);
 
+  /**
+   * @brief Creates graph node relations
+   *
+   * Connects graph operators based on their input and output relations.
+   */
   void CreateNodeRelation();
 
   /**
-   * 初始化kuiper infer计算图节点中的输入操作数
-   * @param inputs pnnx中的输入操作数
-   * @param runtime_operator 计算图节点
+   * @brief Initializes operator inputs
+   *
+   * Initializes the input tensors of a graph operator.
+   *
+   * @param inputs Operator inputs in PNNX graph
+   * @param runtime_operator Runtime graph operator
    */
   static void InitGraphOperatorsInput(
       const std::vector<pnnx::Operand*>& inputs,
       const std::shared_ptr<RuntimeOperator>& runtime_operator);
 
   /**
-   * 初始化kuiper infer计算图节点中的输出操作数
-   * @param outputs pnnx中的输出操作数
-   * @param runtime_operator 计算图节点
+   * @brief Initializes operator outputs
+   *
+   * Initializes the output tensors of a graph operator.
+   *
+   * @param outputs Operator outputs in PNNX graph
+   * @param runtime_operator Runtime graph operator
    */
   static void InitGraphOperatorsOutput(
       const std::vector<pnnx::Operand*>& outputs,
       const std::shared_ptr<RuntimeOperator>& runtime_operator);
 
   /**
-   * 初始化kuiper infer计算图中的节点属性
-   * @param attrs pnnx中的节点属性
-   * @param runtime_operator 计算图节点
+   * @brief Initializes operator attributes
+   *
+   * Initializes attributes of a graph operator.
+   *
+   * @param attrs Operator attributes in PNNX graph
+   * @param runtime_operator Runtime graph operator
    */
   static void InitGraphAttrs(
       const std::map<std::string, pnnx::Attribute>& attrs,
       const std::shared_ptr<RuntimeOperator>& runtime_operator);
 
   /**
-   * 初始化kuiper infer计算图中的节点参数
-   * @param params pnnx中的参数属性
-   * @param runtime_operator 计算图节点
+   * @brief Initializes operator parameters
+   *
+   * Initializes the parameters of a graph operator.
+   *
+   * @param params Operator parameters in PNNX graph
+   * @param runtime_operator Runtime graph operator
    */
   static void InitGraphParams(
       const std::map<std::string, pnnx::Parameter>& params,
       const std::shared_ptr<RuntimeOperator>& runtime_operator);
 
   /**
-   * 根据计算图中的计算节点来返回Layer
-   * @param op 计算图中的计算节点
-   * @return 创建成功的Layer
+   * @brief Creates layer from graph operator
+   *
+   * Creates a Layer object from a graph operator.
+   *
+   * @param op Graph operator
+   * @return Pointer to created Layer object
    */
   static std::shared_ptr<Layer<float>> CreateLayer(
       const std::shared_ptr<RuntimeOperator>& op);
 
   /**
-   * 探查下一层的计算节点
-   * @param current_op 当前计算节点
-   * @param layer_output_data 当前节点的输出，赋予到下一层计算节点的输入张量中
+   * @brief Probes next operator layer
+   *
+   * Feeds the outputs of the current operator to the inputs
+   * of the next operator.
+   *
+   * @param current_op Current operator
+   * @param layer_output_data Outputs of current operator
    */
   static void ProbeNextLayer(
       const std::shared_ptr<RuntimeOperator>& current_op,
       const std::vector<std::shared_ptr<Tensor<float>>>& layer_output_data);
 
  private:
+  /**
+   * @brief Graph state enum
+   */
   enum class GraphState {
     NeedInit = -2,
     NeedBuild = -1,
@@ -170,26 +256,22 @@ class RuntimeGraph {
 
  public:
   /**
-   * 返回模型当前的状态
-   * @return 返回模型当前的状态
+   * @brief Gets current graph state
+   *
+   * @return Current graph state
    */
   GraphState graph_state() const;
 
  private:
+  std::string param_path_;
+  std::string bin_path_;
   int32_t start_forward_index_ = 0;
-  GraphState graph_state_ = GraphState::NeedInit;
-  std::vector<std::shared_ptr<RuntimeOperator>>
-      input_ops_;  /// 计算图中的输入节点
-  std::vector<std::shared_ptr<RuntimeOperator>>
-      output_ops_;  /// 计算图中的输出节点
-
-  std::string param_path_;  /// 计算图的结构文件
-  std::string bin_path_;    /// 计算图的权重文件
-
-  /// 计算图的计算节点
-  std::vector<std::shared_ptr<RuntimeOperator>> operators_;
-  /// pnnx的graph
   std::unique_ptr<pnnx::Graph> graph_;
+
+  GraphState graph_state_ = GraphState::NeedInit;
+  std::vector<std::shared_ptr<RuntimeOperator>> input_ops_;
+  std::vector<std::shared_ptr<RuntimeOperator>> output_ops_;
+  std::vector<std::shared_ptr<RuntimeOperator>> operators_;
 };
 
 }  // namespace kuiper_infer
